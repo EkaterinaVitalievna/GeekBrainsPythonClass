41b06a8115c64e212c4e8badeb882e04a3600d0b
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-10-15 15:26:54 +0300

    Добавили второе решение по поиску числа в строке

--- Seminar_3_2.py
+++ Seminar_3_2.py
@@ -1,6 +1,6 @@
 # 20. Задайте список. Напишите программу, которая определит,
 # присутствует ли в заданном списке строк некое число.
-n = int(input ("Введите число: "))
+n = int(input ("Введите искомое в строке число: "))
 my_list = ["err567", "iu5", "rtu456t", "ffg567"]
 length = len(my_list)
 n = str(n)
@@ -10,3 +10,15 @@ for i in range (length):
         Flag = True
         break
 print (Flag)
+
+lst = ['err567', 'iu5', 'rtu456t', 'ffg567']
+num = int(input('Введите искомое в строке число: '))
+count = 0
+for elem in lst:
+    if str(num) in elem:
+        count += 1
+if count > 0:
+    print ("Да, такое число в строке есть")
+else:
+    print ("Указанное число в строке отсутствует")
+
